bae5402908e497091e48e6daa58bc9282019444e
marcelinhoxx <ihbe@gmx.net>
2026-07-05 14:50:04 +0200

    Update client_http.go

--- internal/growatt_app/client_http.go
+++ internal/growatt_app/client_http.go
@@ -7,49 +7,64 @@ import (
 	"log/slog"
 	"net/http"
 	"net/url"
-	"noah-mqtt/internal/misc"
 	"strings"
 )
 
 func (h *Client) postForm(url string, data url.Values, responseBody any) (*http.Response, error) {
+
 	req, err := http.NewRequest("POST", url, strings.NewReader(data.Encode()))
 	if err != nil {
 		return nil, err
 	}
+
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	if len(h.token) > 0 {
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", h.token))
+
+	if h.token != "" {
+		req.Header.Set("Authorization", "Bearer "+h.token)
 	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/27.0 Chrome/125.0.0.0 Mobile Safari/537.36")
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 10) Noah-MQTT")
+
+	slog.Info("HTTP POST",
+		slog.String("url", url))
+
 	resp, err := h.client.Do(req)
 	if err != nil {
+		slog.Error("HTTP request failed",
+			slog.String("url", url),
+			slog.String("error", err.Error()))
 		return nil, err
 	}
 
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(resp.Body)
+	defer resp.Body.Close()
 
-	b, err := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("request failed: (HTTP %s) %s", resp.Status, string(b))
+	slog.Info("HTTP Response",
+		slog.String("url", url),
+		slog.Int("status", resp.StatusCode),
+		slog.Int("bytes", len(body)))
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("http %d: %s", resp.StatusCode, string(body))
 	}
 
 	if responseBody != nil {
-		if err := json.Unmarshal(b, &responseBody); err != nil {
-			if strings.Contains(err.Error(), "invalid character '<' looking for beginning of value") {
-				if err := h.Login(); err != nil {
-					slog.Error("could not re-login", slog.String("error", err.Error()))
-					misc.Panic(err)
-				}
-				return h.postForm(url, data, responseBody)
-			} else {
-				return nil, err
+
+		if err := json.Unmarshal(body, responseBody); err != nil {
+
+			if strings.Contains(string(body), "<html") ||
+				strings.Contains(string(body), "<!DOCTYPE") {
+
+				slog.Error("Growatt returned HTML instead of JSON")
+
+				return nil, fmt.Errorf("growatt returned html instead of json")
 			}
+
+			return nil, err
 		}
 	}
 
